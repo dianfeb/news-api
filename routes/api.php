a8e590d91ce4980b94509f03d67b7c3da9246788
Dian Febrianto <dianfebrianto41@gmail.com>
2024-04-04 21:43:20 +0700

    api get data category

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Route::get('/articles', function() {
+// dd('tes api');
+// });
+
+Route::get('category', [CategoryController::class, 'index']);
